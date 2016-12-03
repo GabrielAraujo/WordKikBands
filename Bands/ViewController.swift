ea64ef03d13703a1dd752d1fdd39a1d55fd3e70c
Gabriel Araujo <Gabriel.l.arauj@gmail.com>
2016-12-03 14:19:23 -0200

    Initial Commit

---
+++ Bands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bands
+//
+//  Created by Gabriel Araujo on 03/12/16.
+//  Copyright © 2016 Wordkik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
